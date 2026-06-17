e2d77afd820e250db19e80e510494deab8e38e12
Bloxxer126g <tristan.raneroyay@proton.me>
2026-06-17 20:24:50 +0100

    Update Handler

--- api/getTranslation.js
+++ api/getTranslation.js
@@ -2,13 +2,22 @@ import { createClient } from '@supabase/supabase-js'
 const API_SECRET = process.env.SECRET
 const supabase = createClient('https://qwwxjxsfcomnetognqai.supabase.co/', API_SECRET)
 
-export async function getAllPosts() {
+async function getAllPosts() {
     const { data, error } = await supabase.from('Dictionary').select('*');
-    
+
     if (error) {
         console.error('Error fetching posts:', error.message);
         throw error;
     }
 
     return data;
+}
+
+export default async function handler(req, res) {
+    try {
+        const data = await getAllPosts();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 }

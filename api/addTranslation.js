9ef943ca4c776a3a7dc2a59a260dea75c52a2af2
Bloxxer126g <tristan.raneroyay@proton.me>
2026-06-18 20:05:45 +0100

    Nice message v2.0

--- api/addTranslation.js
+++ api/addTranslation.js
@@ -9,11 +9,7 @@ async function addPost(Val) {
     let Gibberish_Word = Info.gibberish_word;
     Dict.forEach(element => {
         if (element["gibberish_word"] == Gibberish_Word) {
-            console.error("Assignment already exists!")
-            throw "Assignment already exists!";
-            err = new error;
-            err.message = "Assignment already exists";
-            return err;
+            throw new Error("Assignment already defined");
         }
     });
 
@@ -24,13 +20,13 @@ async function addPost(Val) {
         throw error;
     }
 
-    return error;
+    return { success: true };
 }
 
 export default async function handler(req, res) {
     try {
         const data = await addPost(req.body);
-        res.status(200).json(data.error || "Added Successfully!");
+        res.status(200).json(data);
     } catch (error) {
         res.status(500).json({ error: error.message || String(error) });
     }

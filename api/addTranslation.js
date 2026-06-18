b5460381db5a7cc26b6847619a6ef66afa073f1a
Bloxxer126g <tristan.raneroyay@proton.me>
2026-06-18 19:50:34 +0100

    Testing :P

--- api/addTranslation.js
+++ api/addTranslation.js
@@ -29,7 +29,7 @@ async function addPost(Val) {
 export default async function handler(req, res) {
     try {
         const data = await addPost(req.body);
-        res.status(200).json(error);
+        res.status(200).json(data);
     } catch (error) {
         res.status(500).json({ error: error.message || error });
     }

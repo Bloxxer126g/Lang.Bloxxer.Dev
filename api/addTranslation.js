f725250400bfa5bc0c854075145cb77fa919a91f
Bloxxer126g <tristan.raneroyay@proton.me>
2026-06-18 19:39:34 +0100

    Testing :P

--- api/addTranslation.js
+++ api/addTranslation.js
@@ -3,6 +3,18 @@ const API_SECRET = process.env.SECRET
 const supabase = createClient('https://qwwxjxsfcomnetognqai.supabase.co/', API_SECRET)
 
 async function addPost(Val) {
+    let Dict = await fetch("/api/getTranslation.js");
+    Dict = await Dict.json();
+    let Info = JSON.parse(Val);
+    let Gibberish_Word = Info.gibberish_word;
+    Dict.body.forEach(element => {
+        if (element.gibberish_word == Gibberish_Word) {
+            console.error("Assignment already exists!")
+            throw "Assignment already exists!";
+            return error;
+        }
+    });
+
     const { error } = await supabase.from('Dictionary').insert(Val);
 
     if (error) {
@@ -16,7 +28,7 @@ async function addPost(Val) {
 export default async function handler(req, res) {
     try {
         const data = await addPost(req.body);
-        res.status(200).json(JSON.stringify(req.body));
+        res.status(200).json("Added successfully");
     } catch (error) {
         res.status(500).json({ error: error.message });
     }

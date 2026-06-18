002aa9bc879fe4296b1d6ae81d146f42c011e2b7
Bloxxer126g <tristan.raneroyay@proton.me>
2026-06-18 19:57:33 +0100

    Updates2

--- api/addTranslation.js
+++ api/addTranslation.js
@@ -32,6 +32,6 @@ export default async function handler(req, res) {
         const data = await addPost(req.body);
         res.status(200).json(data);
     } catch (error) {
-        res.status(500).json({ error: error.message || error });
+        res.status(500).json({ error: error.message || String(error) });
     }
 }
